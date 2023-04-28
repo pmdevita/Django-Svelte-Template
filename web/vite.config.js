54700f522298acd0015f2476e6da1939f5a68c1b
Peter DeVita <mewtwo2643@yahoo.com>
2023-04-28 12:59:02 -0400

    Web: Add Inertia, Tailwind, Example page, configure Vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/static/",
   plugins: [svelte()],
+  build: {
+    outDir: resolve('./dist'),
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js',
+    },
+  },
+  server: {
+    origin: "http://localhost:5173",
+    cors: {
+      allowedHeaders: "*"
+    }
+  }
 })
