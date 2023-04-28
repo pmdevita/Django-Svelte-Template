10b345a3c89a4dd40444752e647c2de73582dc4b
Peter DeVita <mewtwo2643@yahoo.com>
2023-04-28 15:50:23 -0400

    Add SSR configuration, improve Example, add module polyfill, add more instructions to README

--- web/src/pages/Example.svelte
+++ web/src/pages/Example.svelte
@@ -1,1 +1,14 @@
-<p>Hello from Svelte!</p>
+<script>
+    export let inertia = false;
+    let counter = 0;
+    let incCounter = () => counter++;
+</script>
+<p>Hello from Svelte!
+    <span style="color: transparent">
+        <span class="text-blue-500">and Tailwind!</span>
+    </span>
+    {#if inertia}
+        <span>and Inertia!</span>
+    {/if}
+    <button on:click={incCounter}>Counter: {counter}</button>
+</p>
