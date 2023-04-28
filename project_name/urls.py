9434ab26daafc8ad927df869c1f30f8d4523be1b
Peter DeVita <mewtwo2643@yahoo.com>
2023-04-28 15:54:09 -0400

    Django: Add template tags for project name

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_name project.
+URL configuration for {{ project_name }} project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
