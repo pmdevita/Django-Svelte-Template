ee155bb584083d6724d62df9c445673fc6aa0018
Peter DeVita <mewtwo2643@yahoo.com>
2023-06-18 11:04:23 -0400

    Fix secret key missing quotation marks
    (cherry picked from commit 8739587f316e05f475cb10d3e0a392df429cd486)

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = {{ secret_key }}
+SECRET_KEY = "{{ secret_key }}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
